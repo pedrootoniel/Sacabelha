02146a24e8db84ff61f23144c87c6d21059d4afe
pedrootoniel <pedrootonielsantos@outlook.com>
2024-09-19 17:15:23 -0300

    update final 05

--- components/hero-home.tsx
+++ components/hero-home.tsx
@@ -13,7 +13,7 @@ export default function HeroHome() {
               className="animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,theme(colors.gray.200),theme(colors.indigo.200),theme(colors.gray.50),theme(colors.indigo.300),theme(colors.gray.200))] bg-[length:200%_auto] bg-clip-text pb-5 font-nacelle text-4xl font-semibold text-transparent md:text-5xl"
               data-aos="fade-up"
             >
-             SAC ABELHA RAINHA
+           CANAL DE SAC ABELHA RAINHA
             </h1>
             <div className="mx-auto max-w-3xl">
               <p
