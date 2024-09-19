74ec2f3f3740c1617ca1d86418244ad87a7d7186
pedrootoniel <pedrootonielsantos@outlook.com>
2024-09-19 17:03:17 -0300

    update final 04

--- components/hero-home.tsx
+++ components/hero-home.tsx
@@ -13,7 +13,7 @@ export default function HeroHome() {
               className="animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,theme(colors.gray.200),theme(colors.indigo.200),theme(colors.gray.50),theme(colors.indigo.300),theme(colors.gray.200))] bg-[length:200%_auto] bg-clip-text pb-5 font-nacelle text-4xl font-semibold text-transparent md:text-5xl"
               data-aos="fade-up"
             >
-              BEM VINDO AO SAC ABELHA RAINHA
+             SAC ABELHA RAINHA
             </h1>
             <div className="mx-auto max-w-3xl">
               <p
@@ -21,7 +21,7 @@ export default function HeroHome() {
                 data-aos="fade-up"
                 data-aos-delay={200}
               >
-Na Abelha Rainha, nosso compromisso vai além de oferecer produtos de alta qualidade para o cuidado com a sua pele e cabelo.
+              Na Abelha Rainha, nosso compromisso vai além de oferecer produtos de alta qualidade para o cuidado com a sua pele e cabelo.
               </p>
               <div className="mx-auto max-w-xs sm:flex sm:max-w-none sm:justify-center">
                 <div data-aos="fade-up" data-aos-delay={400}>
